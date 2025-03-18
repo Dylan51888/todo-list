8b0ce87ad91e20adb58a0b613771408ccaba0a23
dylan Sun <dylansun@dylans-MacBook-Pro.local>
2025-03-18 15:43:06 +1100

    added all the files

---
+++ resources/js/echo.js
@@ -0,0 +1,16 @@
+import Echo from 'laravel-echo';
+
+import Pusher from 'pusher-js';
+console.log(import.meta.env);
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    cluster: import.meta.env.VITE_REVERB_APP_CLUSTER,
+    // wsHost: import.meta.env.VITE_REVERB_HOST,
+    // wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    // wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
